405f75c757b202944d659dcaeff4adfe4216ef6b
Sergey Ovchinnikov <ovchinnikov.s81@gmail.com>
2019-06-12 12:15:23 +0300

    Solution

---
+++ main.js
@@ -0,0 +1,11 @@
+/* eslint-disable no-undef */
+// eslint-disable-next-line strict
+$('a[href^="#"]').on('click', function(event) {
+  const target = $(this.getAttribute('href'));
+  if (target.length) {
+    event.preventDefault();
+    $('html, body').stop().animate({
+      scrollTop: target.offset().top,
+    }, 500);
+  }
+});
